eca4132c3a44db622d9429ea2ee12f6e5f84bd7d
eak11146 <evo008hp@gmail.com>
2024-11-27 16:34:02 +0700

    fix page

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,15 +1,31 @@
 import Link from "next/link" 
-import dynamic from 'next/dynamic'
- /* import Itemlist from '../component/Itemlist' */
+import Removebtn from '../component/Removebtn'
+import { HiPencilAlt } from 'react-icons/hi';
+  
 
- const DynamicNotFound = dynamic(
-	() => import('../component/Itemlist'),
-	{ ssr: false }
-);
- 
+
+ const getTopics = async () => {
+  try {
+   const res = await fetch("http://localhost:3000/api/topics", {
+     cache: "no-store",
+   });
+
+   if (!res.ok) {
+     throw new Error("Failed to fetch topics");
+   }
+
+   return res.json();
+ } catch (error) {
+   console.log("Error loading topics: ", error);
+ }
+};
+
+
+
 export default async function Productspage() {
 
-
+  const { topics } = await getTopics();
+  
   return (
     <div className="grid justify-items-center">
          
@@ -17,8 +33,29 @@ export default async function Productspage() {
         <Link href ='/addTopic'>
         <button className="bg-white border-solid border-2 border-sky-500 p-3 my-5 rounded-xl ">Add topics</button>
         </Link>
-      {/*   <Itemlist/>  */}
-      <DynamicNotFound />
+        <h1 className='font-bold text-2xl'>List Post</h1>
+        {
+        topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start w-[60%] "
+        >
+          <div>
+            <h2 className="font-bold text-xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
+
+          <div className="flex gap-2">
+            <Removebtn id={t._id} />
+ 
+            <Link href={`/editTopic/${t._id}`}>
+             <HiPencilAlt size={24} /> Edit
+            </Link>
+          </div>
+        </div>
+      ))
+      }
+       
     </div>
   )
 }
