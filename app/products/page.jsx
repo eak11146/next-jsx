206a4e9570ca5830b1056169bfbe8a893b2ead30
eak11146 <evo008hp@gmail.com>
2024-11-27 16:20:03 +0700

    fix1

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link" 
- import Itemlist from '../component/Itemlist'
+import dynamic from 'next/dynamic'
+ /* import Itemlist from '../component/Itemlist' */
 
-
-
+ const DynamicNotFound = dynamic(
+	() => import('../component/Itemlist'),
+	{ ssr: false }
+);
  
-export default async  function Productspage() {
+export default async function Productspage() {
 
 
   return (
@@ -14,7 +17,8 @@ export default async  function Productspage() {
         <Link href ='/addTopic'>
         <button className="bg-white border-solid border-2 border-sky-500 p-3 my-5 rounded-xl ">Add topics</button>
         </Link>
-        <Itemlist/> 
+      {/*   <Itemlist/>  */}
+      <DynamicNotFound />
     </div>
   )
 }
